164082565b6714131d911340126b52706d334233
Shawn_Hyunwoo <107488867+Daniel-HyunWoo@users.noreply.github.com>
2024-12-09 05:21:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    return {
+        "name": "skillsMember",
+        "members": [
+            {
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "name": "level",
+                "type": "int"
+            }
+        ]
+    }
+}
